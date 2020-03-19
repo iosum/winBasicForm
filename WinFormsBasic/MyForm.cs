fd8b7f7dec4ae40a615decfc65bb42770de74c89
Chia-Ying Ho <42313352+iosum@users.noreply.github.com>
2020-03-18 21:31:26 -0400

    inhereit from class

---
+++ WinFormsBasic/MyForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsBasic
+{
+    // Inheriting from class Form
+    class MyForm : Form
+    {
+        // constructor of MyForm
+        public MyForm()
+        {
+            var btn = new Button();
+            btn.Text = "Click Me!";
+
+            // form has a property called Controls
+            // button inherit from Controls
+            // this meaning the current object(MyForm)
+            this.Controls.Add(btn);
+            // 40 down from the form
+            btn.Top = 40;
+            // add an event handler to the event
+            // there is a click event on the button
+            // handler will be invoke, and form.close
+            btn.Click += (s, e) => this.Close();
+        }
+    }
+}
---
+++ WinFormsBasic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var form = new MyForm();
+            // will cause the application.run to exit
+            form.FormClosed += (s, e) => Application.Exit();
+
+
+
+            // show the windows form
+            form.Show();
+
+
+
+            // when the form close, the console still running
+            // continue to accept the input from the user
+            Application.Run();
+        }
+    }
+}
